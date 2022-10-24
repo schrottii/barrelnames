9b02edec204ea40cbfafd27a0ca8e3c29298c4d8
fn78 <oh.seyy@yahoo.de>
2022-10-24 21:13:24 +0200

    aahhh hahgahhaha

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var names1 = names.split("\n");
 var output = "";
 
+var putout = document.getElementById("output");
+
 for (n in names1){
     names1[n].replace(/^[^_]*: /, "")
     output = output + names1[n];
 }
 
-output.innerHTML = output;
+putout.innerHTML = output;
