f5b0f795d016b94a49d9011b2a6acd6e9f4687f6
fn78 <oh.seyy@yahoo.de>
2022-10-24 20:18:13 +0200

    hmm

--- filehandler.js
+++ filehandler.js
@@ -1,20 +1,17 @@
-function fileHandler(input) {
+const file = document.getElementById("file").target.files[0];
+const reader = new FileReader();
 
-    const file = document.getElementById(input);
-    const reader = new FileReader();
+reader.onload = (event) => {
+    const file = event.target.result;
+    const allLines = file.split(/\r\n|\n/);
+    // Reading line by line
+    allLines.forEach((line) => {
+        console.log(line);
+    });
+};
 
-    reader.onload = (event) => {
-        const file = event.target.result;
-        const allLines = file.split(/\r\n|\n/);
-        // Reading line by line
-        allLines.forEach((line) => {
-            console.log(line);
-        });
-    };
+reader.onerror = (event) => {
+    alert(event.target.error.name);
+};
 
-    reader.onerror = (event) => {
-        alert(event.target.error.name);
-    };
-
-    reader.readAsText(file);
-}
+reader.readAsText(file);
