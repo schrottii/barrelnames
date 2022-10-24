28ab34118571b15e227bd2a1a1f78a8c83fb5c20
fn78 <oh.seyy@yahoo.de>
2022-10-24 20:38:36 +0200

    py

--- filemanager.py
+++ filemanager.py
@@ -1,6 +1,12 @@
 thefile = open("file", "r")
 names1 = []
 
-for l in thefile:
+for (l in thefile):
 	names1.append(l)
+	
+	
+thefile.close()
 
+htmli = open('index.html', 'w')
+htmli.write(names1)
+htmli.close()
