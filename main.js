7079a4f19dcd25b4cda39f7c50c9829c745d6c44
fn78 <oh.seyy@yahoo.de>
2022-10-25 07:41:10 +0200

    Genius

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-var names1 = names.split("\n");
+var Names = names.split("\n");
 var output = "";
 
 var putout = document.getElementById("output");
 
-for (n = 0; n < names1.length; n++){
-    names1[n] = names1[n].replace(/^[^_]*: /, "")
+// Remove the numbers (multis have to be removed manually)
+for (n = 0; n < Names.length; n++){
+    Names[n] = Names[n].replace(/^[^_]*: /, "")
 }
 
-output = names1[Math.floor(Math.random() * names1.length)] + " " + names1[Math.floor(Math.random() * names1.length)];
+// Name 1
+var name1 = names1[Math.floor(Math.random() * names1.length)];
+var splittedName = name1.split(" ");
+name1 = splittedName[0]
+for (i = 1; i < splittedName.length; i++) {
+    if (Math.random() > 0.25) name1 = name1 + splittedName[i];
+}
+
+// Name 1
+var name2 = names2[Math.floor(Math.random() * names2.length)];
+var splittedName = name2.split(" ");
+name2 = splittedName[0]
+for (i = splittedName.length - 1; i > -1; i--) {
+    if (Math.random() > 0.25) name2 = name2 + splittedName[i];
+}
+
+output = name1 + " " + name2;
 
 putout.innerHTML = output;
